c72a0e50615ba8bd2fbadc4b68b6b72d7942f449
rafsandsu <37844221+rafsandsu@users.noreply.github.com>
2018-03-27 19:14:31 +0000

    Add files via upload

---
+++ sshcommand.py
@@ -0,0 +1,35 @@
+import pexpect 
+prompt = ['#', '>>>', '>', '\$']
+def send_command(child, cmd):
+    child.sendline(cmd)
+    child.expect(prompt)
+    print child.before
+def connect(user, host, password):
+   ssh_newkey = 'Are you sure you want to continue connecting'
+   connStr = 'ssh ' + user + '@' + host
+   child = pexpect.spawn(connStr)
+   ret = child.expect([pexpect.TIMEOUT, ssh_newkey,\
+                        '[P|p]assword:'])
+
+   if ret == 0:
+    print '[-] Error Connecting'
+    return
+   if ret == 1:
+    child.sendline('yes')
+    ret = child.expect([pexpect.TIMEOUT ,\
+                        '[P|p]assword:'])
+   if ret == 0:
+    print '[-] Err0r Connecting'
+    return
+
+   child.sendline(password)
+   child.expect(prompt)
+   return child
+def main():
+   host = 'localhost'
+   user = 'root'
+   password = 'toor'
+   child = connect(user, host, password)
+   send_command(child, 'cat /etc/shadow | grep root')
+if __name__ == '__main__':
+   main()
